f76363f3b963bb36d5560eac369ff7f353e249ce
Holly Xu <hjxu@uwaterloo.ca>
2013-07-29 21:16:56 -0400

    Add jQuery plugin "gmap3".

--- easygrt/static/easygrt/js/map.js
+++ easygrt/static/easygrt/js/map.js
@@ -1,82 +1,20 @@
+var initialize = function(){
+    var mapOptions = {options :{
+		zoom: 15,
+		center: new google.maps.LatLng(43.47273, -80.541218),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}};
 
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    // Defines the map. "gmap3" is the jQuery plugin for Google Maps
+    $("#map-canvas").gmap3({
+        map: mapOptions,
+        marker: {
+            latLng: [43.47273, -80.541218],
+            options:{
+                draggable: true
+            }
+        }
+    });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-/*
-
-var ourCoords = {
-	latitude: 43.489377399999995,
-	longitude: -79.6871133
-};
-
-window.onload = getMyLocation;
-
-// Called once upon the window load
-// Attempts to locate the user using Geolocation
-function getMyLocation()
-{
-	if(navigator.geolocation) {	
-        // If the navigator geolocation object exists, browser supports API
-		// Pass in a handler function
-		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
-	}
-}
-
-function displayLocation(position)
-{
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
-	
-	var div = document.getElementById("location");
-	div.innerHTML = " You are at latitude " + latitude + " longitude " + longitude;
-	div.innerHTML += " (with " + position.coords.accuracy + "m accuracy)";
-
-	var km = computeDistance(position.coords, ourCoords);
-	var distance = document.getElementById("distance");
-	distance.innerHTML = "You are " + km + " km from Holly";
-
-	showMap(position.coords);	
-}
-
-function showMap(coords)
-{
-	var mapOptions = {
-	  center: new google.maps.LatLng(coords.latitude, coords.longitude),
-	  zoom: 16,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		  
-}
-
-function displayError(error)
-{
-	var errorTypes = 
-	{
-		0: "Unknown error",
-		1: "Permission denied by user",
-		2: "Position is not available",
-		3: "Request timed out"
-	}
-	
-	var errorMessage = errorTypes[error.code];
-	if (error.code == 0 || error.code == 2) {
-		errorMessage = errorMessage + " " + error.message;	
-		// 0 and 2 might have extra info
-	}
-	var div = document.getElementById("location");
-	div.innerHTML = errorMessage;
-}
-
-*/
+$(document).ready(initialize);
