6ed3a2d0335582bd6458aceffae543dee4fc8305
Holly Xu <hjxu@uwaterloo.ca>
2013-09-13 10:19:21 -0400

    Added class to Body tag, fixed map display issue, added POI/Terminal CSS

--- watbus/urls.py
+++ watbus/urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns('',
         url(r'^search/', views.search, name='search'),
         url(r'^map/$', views.map, name='map'),
         url(r'^browse/$', views.browse, name='browse'),
-        url(r'^browse/stops/(?P<stop_id>.+)/$', views.browse_stops, name='browse_stops'),
-        url(r'^browse/trips/(?P<trip_id>.+)/$', views.browse_trips, name='browse_trips'),
-        url(r'^browse/terminals/$', views.browse_all_terminals, name='browse_all_terminals'),
-        url(r'^browse/terminals/(?P<terminal_id>place_.+)/$', views.browse_terminal, name='browse_terminal'),
+        url(r'^browse/stops/(?P<stop_id>.+)/$', views.browse_stops, name='browse'),
+        url(r'^browse/trips/(?P<trip_id>.+)/$', views.browse_trips, name='browse'),
+        url(r'^browse/terminals/$', views.browse_all_terminals, name='terminals'),
+        url(r'^browse/terminals/(?P<terminal_id>place_.+)/$', views.browse_terminal, name='terminals'),
         url(r'^stopjson$', views.stopjson, name='stopjson')
 )
